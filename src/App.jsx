6d86f69191772846e0ee033a14f987923a396c4c
overmind777 <63595243+overmind777@users.noreply.github.com>
2024-02-19 21:46:05 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import Modal from './components/Modal/Modal';
 import TripList from './components/TripList/TripList';
 import WeatherForecast from './components/WeatherForecast/WeatherForecast';
@@ -7,17 +6,18 @@ import { AppWrapper, LeftWrapper } from './style/Global.styled';
 import Button from './components/Button/Button';
 
 function App() {
-
   return (
     <TripsProvider>
       <AppWrapper>
         <LeftWrapper>
+          <h1>Weather Forecast</h1>
+          <input type="date" />
           <TripList />
           <Button />
         </LeftWrapper>
         <WeatherForecast />
-        <Modal />
       </AppWrapper>
+        <Modal />
     </TripsProvider>
   );
 }
--- src/style/Global.styled.js
+++ src/style/Global.styled.js
@@ -4,10 +4,13 @@ export const AppWrapper = styled.div`
   display: flex;
   gap: 20px;
   align-items: center;
+  padding: 20px;
+  background-color: lightgoldenrodyellow;
 `;
 
 export const LeftWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   gap: 20px;
   align-items: center;
   justify-content: space-between;
